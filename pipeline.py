cd3631d3b075041b7a0705e1315bbc637d22d506
Stark16 <pradyumnp508@gmail.com>
2020-03-07 02:02:42 +0530

    Added training.py

--- pipeline.py
+++ pipeline.py
@@ -21,14 +21,15 @@ def pipeline():
 
         # Looping through each image
         for img in os.listdir(path):
-
             # Try catch block to avoid errors cause by broken image or format:
             try:
                 arr = cv2.imread(os.path.join(path, img), 0)
                 dim = arr.shape
-
                 if dim[0] < dim[1]:
                     img_arr = cv2.rotate(arr, cv2.ROTATE_90_CLOCKWISE)
+                img_arr = img_arr[0:int(img_arr.shape[1] / 4), :]
+                #cv2.imshow("gg", img_arr)
+                #cv2.waitKey(0)
                 training_set.append([img_arr, class_num])
 
 
